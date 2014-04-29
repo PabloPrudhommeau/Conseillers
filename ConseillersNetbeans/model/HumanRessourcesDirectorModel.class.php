7ac03a7cb8a109dc42bfd18badf3c18800268de1
Jonathan <Jonathan@PC-CHAMBRE>
2014-04-29 14:23:22 +0200

    Finalisation de l'affichage de tableau

--- ConseillersNetbeans/model/HumanRessourcesDirectorModel.class.php
+++ ConseillersNetbeans/model/HumanRessourcesDirectorModel.class.php
@@ -4,7 +4,7 @@ class HumanRessourcesDirectorModel {
 
 	public function getDatas() {
 		$db = Database::getInstance();
-		$query = $db->query('	SELECT COUNT( c.id_enseignant_chercheur ) AS nbetu, ec.nom, ec.prenom, ec.bureau, lp.libelle
+		$query = $db->query('	SELECT COUNT( c.id_enseignant_chercheur ) AS nbetu, ec.prenom, ec.nom, ec.bureau, lp.libelle
 								FROM conseiller AS c
 								LEFT JOIN enseignant_chercheur AS ec ON ( ec.id = c.id_enseignant_chercheur )
 								LEFT JOIN liste_pole AS lp ON ( lp.id = ec.id_pole )
